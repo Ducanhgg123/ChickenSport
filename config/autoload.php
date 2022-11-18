bdca7cfcd8d79562dd6916dd91dce4cee5cb3a81
Ducanhgg123 <letrongducanh456@gmail.com>
2022-11-18 19:30:33 +0700

    sadsa

--- config/autoload.php
+++ config/autoload.php
@@ -4,6 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 
 header("Access-Control-Allow-Headers: X-Requested-With");
+
 defined('APPPATH') OR exit('Không được quyền truy cập phần này');
 
 /*
